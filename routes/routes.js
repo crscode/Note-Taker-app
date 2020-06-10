ad3bc28ed6824fce8460bbcd11ad57fdc66f1f0a
Christoper Smith <Christophersmithcode@gmail.com>
2020-06-10 15:45:24 -0400

    add and update route.js also made a server.js

---
+++ routes/routes.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = app => {
+
+    // Setup notes variable
+    fs.readFile("db/db.json","utf8", (err, data) => {
+
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+
+        // API ROUTES
+        // ========================================================
+    
+        // Setup the /api/notes get route
+        app.get("/api/notes", function(req, res) {
+            // Read the db.json file and return all saved notes as JSON.
+            res.json(notes);
+        });
+
+        // Setup the /api/notes post route
+        app.post("/api/notes", function(req, res) {
+            // Receives a new note, adds it to db.json, then returns the new note
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log("Added new note: "+newNote.title);
+        });
